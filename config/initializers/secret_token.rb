4c143d74d6e91997aba6a6ffa789fe0638ab9320
Natalie Holton <natalie.k.holton@gmail.com>
2015-12-18 02:10:15 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Freetime::Application.config.secret_key_base = '4e4e1e667e4c93b0d44802a50aa0c4162ef5ea92f09c18b51684cf8add3e40fac88acf573c837db150b91a56c83f21f456d8acd3164fdc0caadfa5d561410cff'
